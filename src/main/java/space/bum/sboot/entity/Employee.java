61ba02509870c0171432bb9ab835a5db7dbee5bd
osparking <jbpark03@gmail.com>
2024-07-10 11:10:20 +0900

    직원 개체 테이블

---
+++ src/main/java/space/bum/sboot/entity/Employee.java
@@ -0,0 +1,11 @@
+package space.bum.sboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    private Long id;
+    private String name;
+}
