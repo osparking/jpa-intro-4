13c602637a48feccd6dd060f9a7167b570615065
osparking <jbpark03@gmail.com>
2024-07-10 16:30:42 +0900

    질의 결과 생성자 사상 정상 작동 - 시험

--- src/test/java/space/bum/sboot/entity/ScheduledDayTest.java
+++ src/test/java/space/bum/sboot/entity/ScheduledDayTest.java
@@ -1,7 +1,9 @@
 package space.bum.sboot.entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,16 @@ class ScheduledDayTest {
     List<Long> employeeIds = (List<Long>) em.createNamedQuery("FridayEmployees")
         .getResultList();
     assertEquals(1, employeeIds.size());
+  }
+  
+  @Test
+  public void whenNamedQuery_thenConstructorResult() {
+    List<ScheduledDay> scheduleDays = Collections.checkedList(
+        em.createNamedQuery("Schedules", ScheduledDay.class).getResultList(),
+        ScheduledDay.class);
+    assertEquals(3, scheduleDays.size());
+    assertTrue(scheduleDays.stream()
+        .allMatch(c -> c.getEmployeeId().longValue() == 100L));
   }
 
 }
