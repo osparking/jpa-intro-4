15131f7f11b07a0d6e58615d7e4430e4590b93c4
osparking <jbpark03@gmail.com>
2024-07-10 09:28:18 +0900

    바엘덩 JPA 학습 - 제 4탄 시작

---
+++ src/main/java/space/bum/sboot/JpaIntro4Application.java
@@ -0,0 +1,13 @@
+package space.bum.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIntro4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaIntro4Application.class, args);
+	}
+
+}
