d3ef4846753fe46de89a2eb1048afb318c310355
osparking <jbpark03@gmail.com>
2024-07-10 11:48:57 +0900

    ColumnResult 는 employeeId 를 사상

--- src/main/java/space/bum/sboot/entity/ScheduledDay.java
+++ src/main/java/space/bum/sboot/entity/ScheduledDay.java
@@ -1,17 +1,22 @@
 package space.bum.sboot.entity;
 
+import jakarta.persistence.ColumnResult;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedNativeQuery;
+import jakarta.persistence.SqlResultSetMapping;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "SCHEDULE_DAYS")
+@SqlResultSetMapping(name = "FridayEmployeeResult", columns = {
+    @ColumnResult(name = "employeeId") })
 public class ScheduledDay {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-    private Long employeeId;
-    private String dayOfWeek;
+  @Id
+  @GeneratedValue
+  private Long id;
+  private Long employeeId;
+  private String dayOfWeek;
 }
