d325d56145a4bd1ab51263fae911574b32d81339
osparking <jbpark03@gmail.com>
2024-07-11 11:33:26 +0900

    유저와 그룹간의 관계에 대한 엔티티 클래스

---
+++ src/main/java/space/bum/sboot/entity/UserGroupRelation.java
@@ -0,0 +1,25 @@
+package space.bum.sboot.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity(name = "r_user_group")
+public class UserGroupRelation implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @Column(name = "user_id", insertable = false, updatable = false)
+  private Long userId;
+
+  @Id
+  @Column(name = "group_id", insertable = false, updatable = false)
+  private Long groupId;
+}
