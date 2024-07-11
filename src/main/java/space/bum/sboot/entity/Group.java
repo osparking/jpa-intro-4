00c9ed41a63119412995bc770a90dfd016ea0171
osparking <jbpark03@gmail.com>
2024-07-11 10:41:08 +0900

    Group/집단 개체

---
+++ src/main/java/space/bum/sboot/entity/Group.java
@@ -0,0 +1,25 @@
+package space.bum.sboot.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Group {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String name;
+
+  @ManyToMany(mappedBy = "groups")
+  private List<User> users = new ArrayList<>();
+}
