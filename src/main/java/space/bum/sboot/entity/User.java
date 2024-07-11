b8fdaef7a529009ba06f484afd99b00d40896213
osparking <jbpark03@gmail.com>
2024-07-11 11:56:29 +0900

    소유측 개체에 JoinTable 애노' 추가

--- src/main/java/space/bum/sboot/entity/User.java
+++ src/main/java/space/bum/sboot/entity/User.java
@@ -6,6 +6,8 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +22,10 @@ public class User {
   private Long id;
   private String name;
 
+  //@formatter:off
   @ManyToMany
+  @JoinTable(name = "r_user_group", 
+    joinColumns = @JoinColumn(name = "user_id"), 
+    inverseJoinColumns = @JoinColumn(name = "group_id"))
   private List<Group> groups = new ArrayList<>();
 }
