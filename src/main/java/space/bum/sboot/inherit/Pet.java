d0e87b26293f91f1c8e8af2d1904a68c63a28534
osparking <jbpark03@gmail.com>
2024-07-12 15:34:46 +0900

    부클래스 주키 열 이름 지정법

--- src/main/java/space/bum/sboot/inherit/Pet.java
+++ src/main/java/space/bum/sboot/inherit/Pet.java
@@ -1,12 +1,14 @@
 package space.bum.sboot.inherit;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "petId")
 public class Pet extends Animal {
   private String name;
 }
