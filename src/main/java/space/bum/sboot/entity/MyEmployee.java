240edbc620ca8ed809f3d242ee94b984714af92b
osparking <jbpark03@gmail.com>
2024-07-12 09:48:37 +0900

    엔티티 클래스인 MyEmployee

---
+++ src/main/java/space/bum/sboot/entity/MyEmployee.java
@@ -0,0 +1,16 @@
+package space.bum.sboot.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyEmployee extends Person {
+    private String company;
+}
