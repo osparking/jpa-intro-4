e48806d14441648cfc7a14e959a792afabde3435
osparking <jbpark03@gmail.com>
2024-07-12 12:10:22 +0900

    각 하위 클래스는 판별식 값을 지정한다

--- src/main/java/space/bum/sboot/entity/Book.java
+++ src/main/java/space/bum/sboot/entity/Book.java
@@ -1,8 +1,10 @@
 package space.bum.sboot.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("1")
 public class Book extends MyProduct {
   private String author;
 }
--- src/main/java/space/bum/sboot/entity/Pen.java
+++ src/main/java/space/bum/sboot/entity/Pen.java
@@ -1,8 +1,11 @@
 package space.bum.sboot.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+
+@DiscriminatorValue("2")
 public class Pen extends MyProduct {
   private String color;
 }
