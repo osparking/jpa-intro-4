c9f9bc9ed3f0656eb9149078851611b8b3c1f28c
osparking <jbpark03@gmail.com>
2024-07-12 18:00:58 +0900

    Pen 생성자 등 추가

--- src/main/java/space/bum/sboot/entity/Pen.java
+++ src/main/java/space/bum/sboot/entity/Pen.java
@@ -2,10 +2,18 @@ package space.bum.sboot.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-
 @DiscriminatorValue("2")
+@Data
+@NoArgsConstructor
 public class Pen extends MyProduct {
   private String color;
+  
+  public Pen(int productId, String name, String color) {
+    super(productId, name);
+    this.color = color;
+  }
 }
