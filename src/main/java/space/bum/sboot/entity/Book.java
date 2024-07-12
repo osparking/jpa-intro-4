181c5e8e9c2eb13ba246a57aa757b4fb2aec1ffb
osparking <jbpark03@gmail.com>
2024-07-12 17:58:41 +0900

    Book 생성자 등 추가

--- src/main/java/space/bum/sboot/entity/Book.java
+++ src/main/java/space/bum/sboot/entity/Book.java
@@ -2,9 +2,18 @@ package space.bum.sboot.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @DiscriminatorValue("1")
+@Data
+@NoArgsConstructor
 public class Book extends MyProduct {
   private String author;
+
+  public Book(int productId, String name, String author) {
+    super(productId, name);
+    this.author = author;
+  }
 }
