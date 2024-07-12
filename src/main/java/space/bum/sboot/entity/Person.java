55060ff091a0b690bce47b4688bef126c8bd50e8
osparking <jbpark03@gmail.com>
2024-07-12 22:35:46 +0900

    수퍼클래스 생성자 부터 해결

--- src/main/java/space/bum/sboot/entity/Person.java
+++ src/main/java/space/bum/sboot/entity/Person.java
@@ -2,10 +2,14 @@ package space.bum.sboot.entity;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @MappedSuperclass
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
   @Id
   private long personId;
